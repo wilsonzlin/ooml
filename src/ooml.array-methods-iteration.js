04195dafdd22aed5fcd0ee7adda313fa04381dc1
Wilson Lin <contact@wilsonl.in>
2016-11-28 16:46:27 +1300

    Added iteration methods

--- src/ooml.array-methods-iteration.js
+++ src/ooml.array-methods-iteration.js
@@ -7,3 +7,21 @@ OOMLArrayProto.findIndex = function() {};
 OOMLArrayProto.forEach = function() {};
 OOMLArrayProto.map = function() {};
 OOMLArrayProto.reduce = function() {};
+OOMLArrayProto.reduceRight = function() {};
+OOMLArrayProto.some = function() {};
+if (typeof Symbol == "function") {
+	OOMLElementProto[Symbol.iterator] = function() {
+		var i = -1,
+			arr = this[OOML_ARRAY_PROPNAME_INTERNALARRAY];
+
+		return {
+			next: function() {
+				if (++i == arr.length) {
+					return { done: true };
+				}
+
+				return { value: arr[i], done: false };
+			}
+		};
+	};
+}
