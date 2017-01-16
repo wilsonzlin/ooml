164be97d1f9a8bf69cd8ec11963c3fb9e742e4d2
Wilson Lin <contact@wilsonl.in>
2017-01-16 18:52:46 +1300

    Prepare for nano branch

--- compile.js
+++ compile.js
@@ -5,7 +5,6 @@ const babel = require('babel-core');
 const args = process.argv.slice(2);
 const DST_ARG = args.find(arg => /^-o=.+$/.test(arg));
 const HARMONY_ARG = args.includes('harmony');
-const NANO_ARG = args.includes('nano');
 
 const DEBUG = args.includes('debug');
 const SRC_DIR = __dirname + '/tmp/';
@@ -13,9 +12,6 @@ const DST_DIR = DST_ARG ? DST_ARG.slice(3) : __dirname + '/dist/';
 
 fs.copySync(__dirname + '/src/', SRC_DIR);
 
-if (NANO_ARG) {
-    console.warn(`========== NANO MODE ==========`);
-}
 if (HARMONY_ARG) {
     console.warn(`========== HARMONY MODE ==========`);
 }
@@ -23,36 +19,24 @@ if (HARMONY_ARG) {
 /*
     OPTIMISATION: Create wrapper function and pass in heavily-used globals for scoped usage minification
 */
-if (NANO_ARG) {
-    fs.writeFileSync(
-        SRC_DIR + 'ooml.js',
+fs.writeFileSync(
+    SRC_DIR + 'ooml.js',
 
-        '(function(Object, undefined) {' +
-            fs.readFileSync(SRC_DIR + 'ooml.js', 'utf8') +
-        '})(Object)'
-    );
-} else {
-    fs.writeFileSync(
-        SRC_DIR + 'ooml.js',
+    '(function(Object, TypeError, SyntaxError, ReferenceError, Error, undefined) {' +
+        '"use strict";' +
+        fs.readFileSync(SRC_DIR + 'ooml.js', 'utf8') +
+    '})(Object, TypeError, SyntaxError, ReferenceError, Error)'
+);
 
-        '(function(Object, TypeError, SyntaxError, ReferenceError, Error, undefined) {' +
-            '"use strict";' +
-            fs.readFileSync(SRC_DIR + 'ooml.js', 'utf8') +
-        '})(Object, TypeError, SyntaxError, ReferenceError, Error)'
-    );
-}
-
-if (!NANO_ARG) {
-    /*
-        OPTIMISATION: Remove the "new" keyword from exceptions
-    */
-    fs.readdirSync(SRC_DIR).forEach(file => {
-        let without_new_keyword = fs.readFileSync(SRC_DIR + file, 'utf8').replace(/new ((?:Syntax|Type|Reference)?Error)/g, (_, errorClass) => {
-            return errorClass;
-        });
-        fs.writeFileSync(SRC_DIR + file, without_new_keyword);
+/*
+    OPTIMISATION: Remove the "new" keyword from exceptions
+*/
+fs.readdirSync(SRC_DIR).forEach(file => {
+    let without_new_keyword = fs.readFileSync(SRC_DIR + file, 'utf8').replace(/new ((?:Syntax|Type|Reference)?Error)/g, (_, errorClass) => {
+        return errorClass;
     });
-}
+    fs.writeFileSync(SRC_DIR + file, without_new_keyword);
+});
 
 if (!DEBUG) {
     /*
@@ -97,6 +81,7 @@ if (!DEBUG) {
 
     // Use alphabetical characters for NodeSet and StringSet minfied hidden properties
     // as some are just a prefix for combination with another number, causing potential overlap
+    // Also, OOML constants use numbers, which might conflict
     propname_autoincrement = -1;
     js_propname_constants_minified = fs.readFileSync(SRC_DIR + 'ooml.NodeSet.js', 'utf8').replace(/'__nodeSet[a-zA-Z]+';$/gm, () => {
         return `'__${ alphabet[++propname_autoincrement] || (() => { throw "Alphabet exceeded" })() }';`;
@@ -110,8 +95,6 @@ if (!DEBUG) {
     fs.writeFileSync(SRC_DIR + 'ooml.StringSet.js', js_propname_constants_minified);
 }
 
-let nano_js_exceptions = [];
-
 zc({
     src: SRC_DIR,
     dst: DST_DIR,
@@ -123,12 +106,6 @@ zc({
     files: ['ooml.js'],
 
     onloadfile: (code) => {
-        if (NANO_ARG) {
-            code = code.replace(/new [A-Za-z]+Error\(`(.+?)`\)/g, (line, errorMessage) => {
-                return nano_js_exceptions.push(errorMessage) - 1;
-            });
-        }
-
         if (!HARMONY_ARG) {
             code = babel.transform(code, {
                 plugins: [
@@ -142,23 +119,6 @@ zc({
         return code;
     },
 });
-
-if (NANO_ARG) {
-    fs.writeFileSync(DST_DIR + 'ooml.nano.exceptions.json', JSON.stringify(nano_js_exceptions));
-    fs.writeFileSync(DST_DIR + 'ooml.nano.exceptions.html', `
-        <table>
-            <thead>
-                <tr style="text-align: left"><th>Code</th><th>Message</th></tr>                
-            </thead>
-            <tbody>
-                ${ nano_js_exceptions.map((msg, id) => `<tr><td>${ id }</td><td>${ msg }</td></tr>`).join('') }            
-            </tbody>
-        </table>
-    `)
-} else {
-    fs.removeSync(DST_DIR + 'ooml.nano.exceptions.json');
-    fs.removeSync(DST_DIR + 'ooml.nano.exceptions.html');
-}
 
 if (!DEBUG) {
     fs.removeSync(SRC_DIR);
