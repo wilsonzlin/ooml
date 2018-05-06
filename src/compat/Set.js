adf5a8cb1a6ae07aff1990ce975e64eb26f7d228
Wilson Lin <contact@wilsonl.in>
2018-05-06 15:48:20 +1200

    Do not use window.Set to detect Set feature compatibility

--- src/compat/Set.js
+++ src/compat/Set.js
@@ -1,4 +1,6 @@
 // IE9 does not have Set
 // IE10 does not have Set.forEach
 // IE11 does not have Set.values
-let __compat_Set = !!window.Set && !!Set.prototype.values;
+// Don't use !!window.Set as this may be used in Node.js
+// Don't use u_typeof as Set may not exist
+let __compat_Set = typeof Set == TYPEOF_FUNCTION && !!Set.prototype.values;
