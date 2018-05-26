d12bfbe3272197f6c7bd97b84d6b993f73c725cb
Wilson Lin <contact@wilsonl.in>
2018-05-26 20:58:16 +1200

    Remove some archived stuff now that bindings have been implemented

--- archive/ooml.js
+++
@@ -1,7 +0,0 @@
-(function (Object, TypeError, SyntaxError, ReferenceError, RangeError, Error, undefined) {
-
-  let BINDING_STATE_INIT = 1;
-  let BINDING_STATE_EXISTS = 2;
-  let BINDING_STATE_MISSING = 3;
-
-})(Object, TypeError, SyntaxError, ReferenceError, RangeError, Error);
