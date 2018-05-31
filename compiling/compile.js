20614685ca0ae77f268c7bb2b5d769a20b308475
Wilson Lin <contact@wilsonl.in>
2018-06-01 00:10:02 +1200

    Add CLI to adjust output directory and prefix

--- compiling/compile.js
+++ compiling/compile.js
@@ -1,20 +1,23 @@
 "use strict";
 
 const referenza = require("referenza");
+const minimist = require("minimist");
 
-const FLAG_CLEAN = process.argv.slice(2).includes("clean");
+const args = minimist(process.argv.slice(2));
+
+const FLAG_CLEAN = args.clean;
 
 const PROJECT_DIR = __dirname + "/../";
 const SOURCE_DIR = PROJECT_DIR + "src/";
 const INTERMEDIATE_DIR = PROJECT_DIR + "tmp/";
-const OUTPUT_DIR = PROJECT_DIR + "dist/";
+const OUTPUT_DIR = args.out || (PROJECT_DIR + "dist/");
 
 const STATE_PATH = PROJECT_DIR + "compiling/state.json";
 
 const PROJECT_NAMES = ["ooml"];
 const METADATA_FILE_NAME = "__metadata__.js";
 
-const URL_PATH_PREFIX = "/docs";
+const URL_PATH_PREFIX = args.prefix || "/docs";
 
 if (FLAG_CLEAN) {
   console.warn(`====================== CLEAN COMPILE ======================`);
