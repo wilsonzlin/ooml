d1c1aeb798d10eb75a57c6333d6b3b44ee8e383a
Wilson Lin <contact@wilsonl.in>
2018-06-04 14:56:05 +1200

    Allow multiple projects (e.g. oomlc-core, oomlvm) to assign to global `ooml` object

--- compile/wrapper.js
+++ compile/wrapper.js
@@ -1,15 +1,20 @@
 ((Object, TypeError, SyntaxError, ReferenceError, RangeError, Error, undefined) => {
   "use strict";
 
+  let ooml;
+  // Don't use u_typeof, as $exports may be undefined
+  if (typeof exports == TYPEOF_OBJECT) {
+    ooml = u_new_clean_object();
+  } else {
+    if (!window.ooml) {
+      ooml = window.ooml = u_new_clean_object();
+    } else {
+      ooml = window.ooml;
+    }
+  }
+
   // Load libooml __main__.js here so only loaded once
   // (can't use zc's importOnce arg due to symlinks)
   __zc_import("../src/__main__.js");
   __zc_import("../../../src/__main__.js");
-
-  // Don't use u_typeof, as $exports may be undefined
-  if (typeof exports ==  TYPEOF_OBJECT) {
-    module.exports = ooml;
-  } else {
-    window.ooml = ooml;
-  }
 })(Object, TypeError, SyntaxError, ReferenceError, RangeError, Error);
