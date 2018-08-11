abe69d06b3a8c6f16eda601031ba4ef3644d5f64
Wilson Lin <contact@wilsonl.in>
2018-08-12 00:45:07 +1200

    Remove archive

--- archive/Utils/createOOMLClass.js
+++
@@ -1,8 +0,0 @@
-let Utils_createOOMLClass = config => {
-  oomlClass[OOML_CLASS_PROPNAME_VIEW_SHAPE] = classViewShapePathToExtensionPoint && classViewShape;
-  oomlClass[OOML_CLASS_PROPNAME_EXTENSIONPOINT_PATH] = classViewShapePathToExtensionPoint;
-  oomlClass[OOML_CLASS_PROPNAME_ROOTELEMTAGNAME] = classViewShape && classViewShape.name;
-  oomlClass[OOML_CLASS_PROPNAME_PROPERTIES_TO_DEPENDENT_BINDINGS] = classPropertiesToDependentBindings;
-
-  return oomlClass;
-};
